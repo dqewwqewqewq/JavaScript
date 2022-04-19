e764496592ae2101c1b57050fb701caccebcef85
dqewwqewqewq <103395215+dqewwqewqewq@users.noreply.github.com>
2022-04-19 16:11:18 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+function total(){
+    var x=document.getElementById("x").value;
+    var y=document.getElementById("y").value;
+    var total=Number(x)+Number(y);/*Give meaning to xy and add*/
+    document.getElementById("result").innerHTML = total;
+    {
+    var x=document.getElementById("x").value;
+    if(x==""||isNaN(x))
+    
+    {
+    
+    alert("No meaning");//If the input is not a number, it will pop up
+    //Marco xiangenyu
+    }
+    }
+    }
